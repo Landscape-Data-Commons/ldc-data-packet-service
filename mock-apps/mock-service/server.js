d4e392fa16589d46816ad0f28f354b012736eb80
Christopher Fraser <cmfraser1380@gmail.com>
2022-07-29 12:39:27 -0600

    Add complete token-processing code

--- mock-apps/mock-service/server.js
+++ mock-apps/mock-service/server.js
@@ -7,6 +7,34 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 require('dotenv').config();
+const jwtok = require('jsonwebtoken')
+
+class MissingScopeError extends Error {
+  constructor () {
+    super()
+    this.code = 'ERR_MISSING_SCOPE'
+  }
+  get name () {
+    return `MissingScopeError [${this.code}]`
+  }
+}
+
+const requiredScopes = {
+  nri:          ['read:NRI'],
+  nwern:        ['read:NWERN'],
+  rhem:         ['read:RHEM'],
+  nriNwern:     ['read:NRI', 'read:NWERN'],
+  nriRhem:      ['read:NRI', 'read:RHEM'],
+  nwernRhem:    ['read:NWERN', 'read:RHEM'],
+  nriNwernRhem: ['read:NRI', 'read:NWERN', 'read:RHEM']
+}
+
+function checkScopes(tokenDecoded, requiredScopes) {
+
+  if (tokenDecoded.scope.split(' ').sort() !== requiredScopes.sort()) {
+    throw new MissingScopeError()
+  }
+}
 
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
   throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
@@ -37,6 +65,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/unrestricted', function (req, res) {
   const primaryKeys = req.body;
   const userId = req.user['http://localhost:3002/email'];
@@ -46,85 +75,154 @@ app.put('/unrestricted', function (req, res) {
   res.status(200).send();
 });
 
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/nri', 
         checkJwt, 
         jwtAuthz(['read:NRI'], { checkAllScopes: true }), 
         function (req, res) {
-  const primaryKeys = req.body;
-  const userId = req.user['http://localhost:3002/email'];
+          try {
+            const tokenDecoded = jwtok.decode(req.headers.authorization.split(' ')[1])
+            checkScopes(tokenDecoded, requiredScopes.nri)
+            const primaryKeys = req.body;
+            const userId = req.user['http://localhost:3002/email'];
+        
+            // CMF: Add query/email/download processing
+        
+          }
+          catch (err) {
+            console.log(err.toString())
+            res.status(403).send();
+          }
 
-  // CMF: Add query-processing and email processing
-
-  res.status(200).send();
+          res.status(200).send();
 });
 
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/nwern', 
         checkJwt, 
-        jwtAuthz(['read:NWERN'], { checkAllScopes: true }), 
+        jwtAuthz(['read:NWERN'], { checkAllScopes: true, failWithError: true }), 
         function (req, res) {
-  const primaryKeys = req.body;
-  const userId = req.user['http://localhost:3002/email'];
-
-  // CMF: Add query-processing and email processing
-
-  res.status(200).send();
+          try {
+            const tokenDecoded = jwtok.decode(req.headers.authorization.split(' ')[1])
+            checkScopes(tokenDecoded, requiredScopes.nwern)
+            const primaryKeys = req.body;
+            const userId = req.user['http://localhost:3002/email'];
+        
+            // CMF: Add query/email/download processing
+        
+          }
+          catch (err) {
+            console.log(err.toString())
+            res.status(403).send();
+          } 
+          
+          res.status(200).send();
 });
 
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/rhem', 
         checkJwt, 
         jwtAuthz(['read:RHEM'], { checkAllScopes: true }), 
         function (req, res) {
-  const primaryKeys = req.body;
-  const userId = req.user['http://localhost:3002/email'];
+          try {
+            const tokenDecoded = jwtok.decode(req.headers.authorization.split(' ')[1])
+            checkScopes(tokenDecoded, requiredScopes.rhem)
+            const primaryKeys = req.body;
+            const userId = req.user['http://localhost:3002/email'];
         
-  // CMF: Add query-processing and email processing
+            // CMF: Add query/email/download processing
         
-  res.status(200).send();
+          }
+          catch (err) {
+            console.log(err.toString())
+            res.status(403).send();
+          } 
+          res.status(200).send();
 });
 
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/nri-nwern', checkJwt, 
         jwtAuthz(['read:NRI', 'read:NWERN'], { checkAllScopes: true }), 
         function (req, res) {
-  const primaryKeys = req.body;
-  const userId = req.user['http://localhost:3002/email'];
-                
-  // CMF: Add query-processing and email processing
-                
-  res.status(200).send();
+          try {
+            const tokenDecoded = jwtok.decode(req.headers.authorization.split(' ')[1])
+            checkScopes(tokenDecoded, requiredScopes.nriNwern)
+            const primaryKeys = req.body;
+            const userId = req.user['http://localhost:3002/email'];
+        
+            // CMF: Add query/email/download processing
+        
+          }
+          catch (err) {
+            console.log(err.toString())
+            res.status(403).send();
+          }   
+
+          res.status(200).send();
 });
  
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/nri-rhem', checkJwt, 
         jwtAuthz(['read:NRI', 'read:RHEM'], { checkAllScopes: true }), 
         function (req, res) {
-  const primaryKeys = req.body;
-  const userId = req.user['http://localhost:3002/email'];
-                
-  // CMF: Add query-processing and email processing
-                
-  res.status(200).send();
+          try {
+            const tokenDecoded = jwtok.decode(req.headers.authorization.split(' ')[1])
+            checkScopes(tokenDecoded, requiredScopes.nriRhem)
+            const primaryKeys = req.body;
+            const userId = req.user['http://localhost:3002/email'];
+        
+            // CMF: Add query/email/download processing
+        
+          }
+          catch (err) {
+            console.log(err.toString())
+            res.status(403).send();
+          } 
+
+          res.status(200).send();
 });
 
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/nwern-rhem', checkJwt, 
         jwtAuthz(['read:NWERN', 'read:RHEM'], { checkAllScopes: true }), 
         function (req, res) {
-  const primaryKeys = req.body;
-  const userId = req.user['http://localhost:3002/email'];
-                
-  // CMF: Add query-processing and email processing
-                
-  res.status(200).send();
+          try {
+            const tokenDecoded = jwtok.decode(req.headers.authorization.split(' ')[1])
+            checkScopes(tokenDecoded, requiredScopes.nwernRhem)
+            const primaryKeys = req.body;
+            const userId = req.user['http://localhost:3002/email'];
+        
+            // CMF: Add query/email/download processing
+        
+          }
+          catch (err) {
+            console.log(err.toString())
+            res.status(403).send();
+          }       
+
+          res.status(200).send();
 });
 
+// 2022-07-29-CMF: NOTE THAT { checkAllScopes: true } *DOES NOT CHECK ALL SCOPES; LEFT IN ANYWAY; REMOVE IF DESIRED
 app.put('/nri-nwern-rhem', 
         checkJwt, 
         jwtAuthz(['read:NRI', 'read:NWERN', 'read:RHEM'], { checkAllScopes: true }), 
         function (req, res) {
-  const primaryKeys = req.body;
-  const userId = req.user['http://localhost:3002/email'];
-                
-  // CMF: Add query-processing and email processing
-                
-  res.status(200).send();
+          try {
+            const tokenDecoded = jwtok.decode(req.headers.authorization.split(' ')[1])
+            checkScopes(tokenDecoded, requiredScopes.nriNwernRhem)
+            const primaryKeys = req.body;
+            const userId = req.user['http://localhost:3002/email'];
+
+            // CMF: Add query/email/download processing
+
+          }
+          catch (err) {
+            console.log(err.toString())
+            res.status(403).send();
+          }
+
+          res.status(200).send();
 });
 
 // launch the API Server at localhost:8090
