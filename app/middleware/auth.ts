d7ca9a3d9e7b1090e21cc10a5559c4e6103ccc2a
Kris! <42949874+krstphrrr@users.noreply.github.com>
2022-08-08 12:57:11 -0600

    unexpected audience

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -7,9 +7,10 @@ import {
 } from 'express-oauth2-jwt-bearer';
 import secrets from '../db/secrets'
 
+let aud = secrets.read('node_auth0audience')||process.env.AUTH0_AUDIENCE
 let domain = secrets.read('node_auth0domain')||process.env.AUTH0_DOMAIN
 export const authCheck = auth({
   issuerBaseURL: `https://${domain}`,
   // jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-  audience: secrets.read('node_auth0audience')||process.env.AUTH0_AUDIENCE
+  audience: aud
 })
